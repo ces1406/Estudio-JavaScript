9252db08dfbf623fc0ebeb93edb4dd0ca141f9f0
cesar montalvan <ces1406@gmail.com>
2020-10-07 11:21:39 -0300

    agregando algo de scope

---
+++ scope/scope.js
@@ -0,0 +1,42 @@
+var global =' SOY GLOBAL '
+
+function scope1 (){
+    var a1 = [1,2,3];   
+    console.log('scope1()->global: '+global) 
+    for(var i1=0;i1<a1.length;i1++){
+        var value1 = a1[i1];
+        console.log('value1: '+value1)
+    }
+    console.log("Fuera del for->value: "+value1);
+    // o sea que value está disponible a un scope superior?
+    // ( o en realidad "for" no está determinando ningun scope)
+};
+scope1();
+
+function scope2 (){
+    var a2 = [1,2,3];    
+    for(var i2=0;i2<a2.length;i2++){
+        let value2 = a2[i2];        
+        console.log('value2: '+value2)
+    }
+    //console.log("Fuera del for->value: "+value ) // -->ahora da ERROR
+};
+scope2();
+
+function scope3 (){
+    var a3 = 2;    
+    let b3 = 1;
+    {
+        var c3 = '**scope3->{c3}**';
+        let v3 = 'a';
+        console.log('Dentro de {} -> a3: '+ a3 )
+        console.log('Dentro de {} -> b3: '+ b3 )
+    }
+    console.log("Fuera del {} -> c3: " + c3 )
+    // let b3 = 8; // --> da ERROR (pero con "value2" en el for del scope2 no?)
+    //console.log("Fuera del {} -> v: "+v ) // -->da ERROR
+};
+scope3();
+
+//console.log("Fuera del todo -> a1: " + a1); //--> da ERROR
+//console.log("Fuera del todo -> value1: " + value1); //--> da ERROR
--- this/this.js
+++ this/this.js
@@ -4,17 +4,26 @@ const animal = {
     ruido() { console.log('beeee'); },
     alimento() { console.log('dieta: ' + dieta) },                      // da error    
     alimento2() { console.log('dieta: ' + this.dieta) },                // no da error    
-    alimento3: function () { console.log('dieta: ' + this.dieta) },     // no da error    
+    alimento3: function () { console.log('dieta: ' + this.dieta) },     // no da error --> segun muchos libros tendría que dar undefined   
     //function alimento4 () { console.log('dieta:'+this.dieta)}, //--->definicion erronea (no se puede definir así)
     cumple: () => { console.log('come: ' + dieta) },                    // da error tambien aunque sea una arrow function    
     cumple2: () => { console.log('come: ' + this.dieta) }               // da "undefined"
 }
-animal.ruido();
+console.log('-----------------------------')
+console.log("\x1b[34m%s\x1b[0m", 'animal.ruido() ');
+animal.ruido()
+console.log('-----------------------------')
 //animal.alimento(); // comentado por provocar error
+console.log("\x1b[34m%s\x1b[0m", 'animal.alimento2()')
 animal.alimento2();
+console.log('-----------------------------')
+console.log("\x1b[34m%s\x1b[0m", 'animal.alimento3()')
 animal.alimento3();
+console.log('-----------------------------')
 //animal.cumple(); // comentado por provocar error
-animal.cumple2()
+console.log("\x1b[34m%s\x1b[0m", 'animal.cumple2() ');
+animal.cumple2();
+console.log('-----------------------------')
 
 const adornar = () => { console.log('-->' + this.nombre + '<--') }
 function Persona(nombre) {
@@ -29,10 +38,35 @@ function Persona2(nombre) {
         console.log('esto: ', esto)
     }
 }
-animal.alimento2();
+
 const persona1 = new Persona('jhon');
 persona1();
 console.log('->persona1 es un new Persona()=> se creó algo?->persona1: ');
 console.log(persona1); // -->persona1 es la funcion que retorno Persona (???)
 const persona2 = new Persona2('john');
 persona2();
+
+class Contador {
+    constructor(inicio,fin){
+        this.start = inicio;
+        this.end = fin
+    }
+    diferencia(){
+        console.log('dif: '+this.start+' - '+this.end)
+    }
+    adornar = function (){
+        console.log(this.start+'***'+this.end)
+    }
+    decorar =()=>{
+        console.log(this.start+'*++*'+this.end)
+    }
+}
+var cont = new Contador(2,4)
+cont.diferencia()
+cont.adornar()
+cont.decorar()
+
+const fx = cont.decorar;
+fx();
+const ff = animal.alimento2;
+ff()
