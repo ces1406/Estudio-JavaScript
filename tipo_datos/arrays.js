d6dc147c53c790a68b33d679f26dcfc42c7faeba
cesar montalvan <ces1406@gmail.com>
2020-10-08 09:21:15 -0300

    updating arrays

--- tipo_datos/arrays.js
+++ tipo_datos/arrays.js
@@ -13,7 +13,7 @@ console.log('array1.toString(): '+array1.toString())
 console.log("\x1b[35m%s\x1b[0m", '-------------------------------------------array.join()----------------------------------------------------')
 console.log('array2.join("-"): '+array2.join('-'))
 console.log('array1.join("-"): '+array1.join('-'))
-//------------------------------------array.push()-----array.pop()-------------------------------------------
+//-------------------------array.push() : agrega al final del array-----array.pop() : saca el último elemento-----------------------------
 console.log("\x1b[35m%s\x1b[0m", '------------------------------------array.push()-----array.pop()-------------------------------------------')
 console.log('array2(ANTES DE->pop): ',array2)
 let ultimoSacado = array2.pop()
@@ -22,7 +22,7 @@ console.log('array2(LUEGO DE->pop): ',array2)
 let longArray = array2.push({name:'PEQUE',edad:99})
 console.log('Longitud array-push()ed: ',longArray)
 console.log('array2(LUEGO DE->push): ',array2)
-//----------------------------------array.shift()-----array.unShift()---------------------------------------
+//-------------------------array.shift() : saca el 1er. elemento-----array.unShift() : agrega al inicio del array------------------------
 console.log("\x1b[35m%s\x1b[0m", '----------------------------------array.shift()-----array.unShift()---------------------------------------')
 let primeroSacado = array2.shift();
 console.log('Elemento shift()ed: ',primeroSacado);
@@ -56,17 +56,20 @@ console.log('Buscar "coco"->encontrar("coco"): ',encontrar('coco'))
 
 
 
-//--------splice(posDondeAgregar,cantElementosARemover,elementoAAgregar1,elementoAAgregar2,....)-----------
+//-----unArray.splice(posDondeAgregar,cantElementosARemover,elementoAAgregar1,elementoAAgregar2,....)-------
 console.log("\x1b[35m%s\x1b[0m", '--------splice(posDondeAgregar,cantElementosARemover,elementoAAgregar1,elementoAAgregar2,....)-----------')
 console.log('array2(ANTES DE->splice): ',array2)
+personas = [{name:'Lautardo',edad:30},{name:'Sermgeco',edad:20},{name:'Barado',edad:10}]
 array2.splice(2,1,{name:'Armando',edad:0})
 console.log('array2(LUEGO DE->splice): ',array2)
+array2.splice(2,0,personas);
+console.log('array2(LUEGO DE->splice(2,0,arrayDepersonas)): ',array2)
 //-----------------------------var unArray = array.concat(arra1,arra2,.....)-------------------------------
 console.log("\x1b[35m%s\x1b[0m", '-----------------------------var unArray = array.concat(arra1,arra2,.....)-------------------------------')
 var unArray = array2.concat([{name:'zaaaa',edad:1},{name:'zbbbbb',edad:2}],[{name:'Zaaaa',edad:3},{name:'Zbbbbb',edad:3}],[{name:'Yzaaaa',edad:5},{name:'Yzbbbbb',edad:5}])
 console.log('unArray: ',unArray)
-//--------------------------------var unArray = array.slice(posAPartirDeDondeCortar)-----------------------------------
-console.log("\x1b[35m%s\x1b[0m", '--------------------------------var unArray = array.slice(posAPartirDeDondeCortar)-----------------------------------')
+//------------------------------var unArray = array.slice(posInicio,posFinal)------------------------------
+console.log("\x1b[35m%s\x1b[0m", '-----------------------------var unArray = array.slice(posInicio,posFinal)-------------------------------')
 let array3 = array2.slice();
 console.log('array3 = array2.slice() : ',array3)
 console.log('array3.slice(1): ',array3.slice(1))
@@ -74,9 +77,10 @@ console.log('array3.slice(1): ',array3.slice(1))
 
 
 //-------------------------------------------array.reverse()-----------------------------------------------
-console.log("\x1b[35m%s\x1b[0m", '-------------------------------------------array.reverse()-----------------------------------------------')
+console.log("\x1b[35m%s\x1b[0m", '--------------------------------unArray = array.reverse()-----------------------------------------------')
 console.log('array1: '+JSON.stringify(array1))
 console.log('array1.reverse(): '+JSON.stringify(array1.reverse()))
+console.log('array2.reverse(): '+JSON.stringify(array2.reverse()))
 //---------------------------------------array.sort()---ORDEN ALFABETICO-----------------------------------
 console.log("\x1b[35m%s\x1b[0m", '---------------------------------------array.sort()---ORDEN ALFABETICO-----------------------------------')
 //console.log('array1: ',array1)
